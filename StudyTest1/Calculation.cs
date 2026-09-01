10aa675955a69f94b47007947891e79d386eeac0
ken.sasaki <sasaki_ks@yahoo.co.jp>
2026-09-01 16:20:40 +0900

    コミット

---
+++ StudyTest1/Calculation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyTest1
+{
+    public class Calculation
+    {
+        public int Add(int a, int b)
+        {
+            return (a + b);
+        }
+    }
+}
---
+++ TestProject1/CalculationTest.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using StudyTest1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject1
+{
+    public class CalculationTest
+    {
+        [Test]
+        public void AddTest()
+        {
+            Calculation c = new Calculation();
+            int ret = c.Add(3, 4);
+
+            Assert.AreEqual(6, ret);
+        }
+    }
+}
