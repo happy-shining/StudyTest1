239add8367019d4348982e1c021f48573ecb4230
ken.sasaki <sasaki_ks@yahoo.co.jp>
2026-09-02 14:55:44 +0900

    メッセージ１０にしました

--- StudyTest1/Form1.cs
+++ StudyTest1/Form1.cs
@@ -19,7 +19,7 @@ namespace StudyTest1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("クリックしました９");
+            MessageBox.Show("クリックしました１０");
         }
     }
 }
