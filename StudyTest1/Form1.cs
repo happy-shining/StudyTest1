5876ae39724d072b52395d3bd605660bcf713156
ken.sasaki <sasaki_ks@yahoo.co.jp>
2026-09-02 13:42:04 +0900

    メッセージ８にしてみました

--- StudyTest1/Form1.cs
+++ StudyTest1/Form1.cs
@@ -19,7 +19,7 @@ namespace StudyTest1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("クリックしました7");
+            MessageBox.Show("クリックしました８");
         }
     }
 }
